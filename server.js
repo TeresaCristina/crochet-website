a5151d75919cea50673e74d2669499de4d50c8cb
TeresaCristina <tcpaixao-costa@myseneca.ca>
2021-04-21 11:15:00 -0400

    fixing server.js

--- server.js
+++ server.js
@@ -5,28 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/itch-for-stitch'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/crochet-website'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/crochet-website/index.html'));
+res.sendFile(path.join(__dirname+'/dist/itch-for-stitch/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
